a5cbb231cddf284f4950c1bdbfc0f2095802c8ea
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-01-22 18:50:02 +0100

    added refresh button

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
 const loading = document.getElementById("spinner");
 const mail = document.querySelectorAll(".email-item");
 const mailList = document.querySelector(".list-group");
-console.log(mail);
+const refresh = document.getElementById("refresh-button");
 
 for (let i = 0; i < 10; i++) {
   axios
     .get("https://flynn.boolean.careers/exercises/api/random/mail")
     .then((response) => {
-      console.log(response);
-
       const randomMail = response.data.response;
-      console.log(randomMail);
+
       mail[i].innerHTML += randomMail;
       mailList.classList.remove("d-none");
     })
@@ -18,6 +16,23 @@ for (let i = 0; i < 10; i++) {
       console.log(error);
     })
     .finally(() => {
-      loading.classList.add("d-none");
+      if (mail[9] !== "") {
+        loading.classList.add("d-none");
+      }
     });
 }
+
+refresh.addEventListener("click", function () {
+  loading.classList.remove("d-none");
+  for (let i = 0; i < 10; i++) {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((response) => {
+        const randomMail = response.data.response;
+        mail[i].innerHTML = randomMail;
+      })
+      .finally(() => {
+        loading.classList.add("d-none");
+      });
+  }
+});
