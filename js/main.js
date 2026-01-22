14d03cd916d31fa6dfaa7d01fcd73d0568810f0a
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-01-22 13:58:15 +0100

    wrote code for random mail

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-axios;
+axios
+  .get("https://flynn.boolean.careers/exercises/api/random/mail")
+  .then((response) => {
+    console.log(response);
+
+    const randomMail = response.data.response;
+    console.log(randomMail);
+  });
